c6aab63eeae3dcce4f0505a25ebe0ea208b209de
Otavio Araujo <otavio-luiz17@outlook.com>
2019-02-13 16:03:21 -0200

    Project

---
+++ CarRentals/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace CarRentals.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ CarRentals/Services/ITaxService.cs
@@ -0,0 +1,7 @@
+﻿namespace CarRentals.Services
+{
+    interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
